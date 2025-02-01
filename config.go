f3d11f542c06b03e3f2740b9094ceca48c9fc397
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2025-02-01 20:32:45 +0700

    feat: implement config and its option when create pgx pool

---
+++ config.go
@@ -0,0 +1,79 @@
+package pgxtxpool
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type config struct {
+	dsn   url.URL
+	query url.Values
+}
+
+// ParseToPGXConfig will parse dsn and query URL to pgxpool config
+func (c *config) ParseToPGXConfig() *pgxpool.Config {
+	c.dsn.Scheme = "postgres"
+	c.dsn.RawQuery = c.query.Encode()
+	config, err := pgxpool.ParseConfig(c.dsn.String())
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
+
+// Option is a function that can be used to configure the pgxpool
+type Option func(*config)
+
+// SetHost will set postgres host and port
+func SetHost(host, port string) Option {
+	return func(c *config) {
+		c.dsn.Host = fmt.Sprintf("%s:%s", host, port)
+	}
+}
+
+// SetCredential will set postgres user and password
+func SetCredential(user, password string) Option {
+	return func(c *config) {
+		c.dsn.User = url.UserPassword(user, password)
+	}
+}
+
+// SetDatabase will set postgres database
+func SetDatabase(database string) Option {
+	return func(c *config) {
+		c.dsn.Path = database
+	}
+}
+
+// WithSSLMode will set sslmode
+func WithSSLMode(mode string) Option {
+	return func(c *config) {
+		c.query.Set("sslmode", mode)
+	}
+}
+
+// WithMaxConns will set max connections
+func WithMaxConns(maxConns int) Option {
+	return func(c *config) {
+		c.query.Set("pool_max_conns", fmt.Sprintf("%d", maxConns))
+	}
+}
+
+// WithMaxIdleConns will set max idle connections
+// ex: "30s", "5m"
+func WithMaxIdleConns(maxIdleConns string) Option {
+	return func(c *config) {
+		c.query.Set("pool_max_conn_idle_time", maxIdleConns)
+	}
+}
+
+// WithMaxConnLifetime will set max connection lifetime
+// ex: "30s", "5m"
+func WithMaxConnLifetime(maxConnLifetime string) Option {
+	return func(c *config) {
+		c.query.Set("pool_max_conn_lifetime", maxConnLifetime)
+	}
+}
