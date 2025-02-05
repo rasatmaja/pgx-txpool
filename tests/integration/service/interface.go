d5eb319cc923df71b35b2aee6b5f054d9375bc9b
Rasio Atmaja <rasioatmaja29@gmail.com>
2025-02-05 16:39:30 +0700

    test: skeleton integration test (#5)

---
+++ tests/integration/service/interface.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"context"
+
+	"github.com/rasatmaja/pgx-txpool/tests/integration/model"
+)
+
+// Repository ---
+type Repository interface {
+	BeginTx(ctx context.Context) (context.Context, error)
+	CommitTx(ctx context.Context) error
+	RollbackTx(ctx context.Context) error
+	CreateUser(ctx context.Context, user model.User) error
+	UpdateUserBalance(ctx context.Context, user model.User) error
+	CreateTransaction(ctx context.Context, transactions []model.Transaction) error
+	CreateTransactionTransfer(ctx context.Context, transactions []model.TransactionTransfer) error
+}
