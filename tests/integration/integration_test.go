3962bde273682180b496b8a82b52ccad5c53b0fa
Rasio Atmaja <rasioatmaja29@gmail.com>
2025-02-09 19:39:32 +0700

    test: add cases create user flow (#8)

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -25,20 +25,92 @@ func TestMain(m *testing.M) {
 // TestCreateUser tests service CreateUser method
 func TestCreateUser(t *testing.T) {
 	ctx := context.Background()
-	user := model.User{
-		ID:      "123",
-		Name:    "test",
-		Balance: 100,
+	cases := []struct {
+		name  string
+		error bool
+		user  model.User
+		trx   []model.Transaction
+	}{
+		{
+			name:  "USR001: should success",
+			error: false,
+			user: model.User{
+				ID:      "USR001",
+				Name:    "John Doe",
+				Balance: 1000,
+			},
+			trx: []model.Transaction{
+				{
+					ID:     "TRX001",
+					UserID: "USR001",
+					Type:   "INITIAL_BALANCE",
+					Amount: 1000,
+				},
+			},
+		},
+		{
+			name:  "USR002: should rollback user data when create trx error",
+			error: true,
+			user: model.User{
+				ID:      "USR002",
+				Name:    "Jane",
+				Balance: 2000,
+			},
+			trx: []model.Transaction{
+				{
+					ID:     "TRX002",
+					UserID: "XXXXX",
+					Type:   "INITIAL_BALANCE",
+					Amount: 2000,
+				},
+			},
+		},
+		{
+			name:  "USR003: should success",
+			error: false,
+			user: model.User{
+				ID:      "USR003",
+				Name:    "Peterson",
+				Balance: 3000,
+			},
+			trx: []model.Transaction{
+				{
+					ID:     "TRX003",
+					UserID: "USR003",
+					Type:   "INITIAL_BALANCE",
+					Amount: 3000,
+				},
+			},
+		},
+		{
+			name:  "USR004: should success",
+			error: false,
+			user: model.User{
+				ID:      "USR004",
+				Name:    "Waller",
+				Balance: 4000,
+			},
+			trx: []model.Transaction{
+				{
+					ID:     "TRX004",
+					UserID: "USR004",
+					Type:   "INITIAL_BALANCE",
+					Amount: 4000,
+				},
+			},
+		},
 	}
-	// TODO: test case user goes here
-	t.Run("should XOXO", func(t *testing.T) {
-		t.Parallel()
 
-		err := srv.CreateUser(ctx, user)
-		if err != nil {
-			t.Errorf("failed to create user: %v", err)
-		}
-	})
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := srv.CreateUser(ctx, c.user, c.trx...)
+			if err != nil {
+				t.Errorf("failed to create user: %v", err)
+			}
+		})
+	}
 
 	t.Run("check data integrity", func(t *testing.T) {
 		t.Parallel()
